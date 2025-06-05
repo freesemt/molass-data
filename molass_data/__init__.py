0e96c98d95d9acc120e82dfbf86cc39e0068600d
freesemt <freesemt@gmail.com>
2025-06-05 13:00:26 +0900

    Refactor to allow other subfolders

--- molass_data/__init__.py
+++ molass_data/__init__.py
@@ -9,12 +9,12 @@ def get_version(toml_only=False):
     from molass import _get_version_impl
     return _get_version_impl(toml_only, __file__, __package__)
 
-def get_tutorial_data_path():
+def get_data_path(subfolder):
     """
     Returns the path to the tutorial data directory.
     """
     import os
     this_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(this_dir, "tutorial_data")
+    return os.path.join(this_dir, subfolder)
 
-TUTORIAL_DATA = get_tutorial_data_path()
+TUTORIAL_DATA = get_data_path("tutorial_data")
